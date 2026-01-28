692e7eb5b5c7ea5afcd8de28a69a9cf3711b2725
Shivam Kumar <145775372+ShivamKumarPTU@users.noreply.github.com>
2026-01-28 23:14:10 +0530

    Add Hilt plugin to Gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
+// This comment is added to force a Gradle cache invalidation.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.google.gms.google.services) apply false
     alias(libs.plugins.ksp) apply false
-
+    alias(libs.plugins.hilt) apply false
 }
